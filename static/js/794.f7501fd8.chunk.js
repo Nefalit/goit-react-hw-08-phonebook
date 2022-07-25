8841fb0019c3fb55836ffeee2c88e977fd89e7a8
Nefalit <97884036+Nefalit@users.noreply.github.com>
2022-07-25 09:57:06 +0000

    Deploying to gh-pages from @ Nefalit/goit-react-hw-08-phonebook@62daecf8f36ac90d4256986aa7f4901b07fdb0f6 🚀

---
+++ static/js/794.f7501fd8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[794],{1794:function(e,n,o){o.r(n);var c=o(3504),h=o(3329);n.default=function(){return(0,h.jsx)("main",{children:(0,h.jsxs)("div",{className:"container",children:[(0,h.jsx)("h2",{children:"Page not found"}),(0,h.jsx)(c.rU,{to:"/",children:"To home page"})]})})}}}]);
+//# sourceMappingURL=794.f7501fd8.chunk.js.map
