72f06225402f039e65c75a55764f5cc97be26ee8
Nefalit <97884036+Nefalit@users.noreply.github.com>
2022-07-25 16:34:43 +0000

    Deploying to gh-pages from @ Nefalit/goit-react-hw-08-phonebook@d5850d42fcdbac4a22cd351c307f0be5bf680b50 🚀

---
+++ static/js/418.751e2cdb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[418],{9418:function(e,o,n){n.r(o),n.d(o,{default:function(){return t}});var a="home-page_title__fc3KH",c="home-page_imgWrapper__jwZWk",r=n(3329),t=function(){return(0,r.jsx)("main",{children:(0,r.jsxs)("div",{className:"container",children:[(0,r.jsx)("h1",{className:a,children:"Welcome to create your phonebook!!!"}),(0,r.jsx)("div",{className:c})]})})}}}]);
+//# sourceMappingURL=418.751e2cdb.chunk.js.map
--- static/js/680.49329d03.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,n,c){c.r(n);var o=c(3329);n.default=function(){return(0,o.jsx)("main",{children:(0,o.jsx)("div",{className:"container",children:(0,o.jsx)("h2",{children:"Home Page"})})})}}}]);
-//# sourceMappingURL=680.49329d03.chunk.js.map
